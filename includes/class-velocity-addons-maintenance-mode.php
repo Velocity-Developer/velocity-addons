26dbf8bc18748daeebf909017f2aaaf70ad696c9
aadiityaak <aadiityaak@gmail.com>
2023-06-08 11:21:54 +0700

    Menambah fungsi maintenance mode

---
+++ includes/class-velocity-addons-maintenance-mode.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Register all actions and filters for the plugin
+ *
+ * @link       https://velocitydeveloper.com
+ * @since      1.0.0
+ *
+ * @package    Velocity_Addons
+ * @subpackage Velocity_Addons/includes
+ */
+
+/**
+ * Register all actions and filters for the plugin.
+ *
+ * Maintain a list of all hooks that are registered throughout
+ * the plugin, and register them with the WordPress API. Call the
+ * run function to execute the list of actions and filters.
+ *
+ * @package    Velocity_Addons
+ * @subpackage Velocity_Addons/includes
+ * @author     Velocity <bantuanvelocity@gmail.com>
+ */
+class Velocity_Addons_Maintenance_Mode {
+    public function __construct() {
+        if (get_option('maintenance_mode')) {
+            add_action('wp', array($this, 'check_maintenance_mode'));
+        }
+    }
+
+    public function check_maintenance_mode() {
+        if (!current_user_can('manage_options') && !is_admin()) {
+            wp_die('<h1>Maintenance Mode</h1><p>We are currently performing maintenance. Please check back later.</p>', 'Maintenance Mode');
+        }
+    }
+}
+
+// Inisialisasi class Velocity_Addons_Maintenance_Mode
+$velocity_maintenance_mode = new Velocity_Addons_Maintenance_Mode();
--- includes/class-velocity-addons.php
+++ includes/class-velocity-addons.php
@@ -127,6 +127,11 @@ class Velocity_Addons {
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-velocity-addons-limit-login-attempts.php';
 
 		/**
+		 * Berisi Class untuk fungsi maintenance mode.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-velocity-addons-maintenance-mode.php';
+
+		/**
 		 * The class responsible for defining all actions that occur in the admin area.
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-velocity-addons-admin.php';
