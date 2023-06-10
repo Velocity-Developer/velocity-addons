c0f381aa1d9266cc61ad6b0bb390423518a9abe2
aadiityaak <aadiityaak@gmail.com>
2023-06-10 08:36:32 +0700

    Move update checker to lib

--- velocity-addons.php
+++ velocity-addons.php
@@ -71,7 +71,7 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-velocity-addons.php';
 /**
  * The core plugin class that is used to cek update and run auto update.
  */
-require 'includes/plugin-update-checker/plugin-update-checker.php';
+require 'lib/plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 $myUpdateChecker = PucFactory::buildUpdateChecker(
 	'https://velocitydeveloper.id/auto-update/plugins/velocity-addons/info.json',
