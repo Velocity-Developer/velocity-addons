f3c50cb3b167ed2e83133067b48ef455a7f7d92a
adityathok <aditya0thok@gmail.com>
2023-06-12 09:13:23 +0700

    exclude page myaccount from maintenance mode

--- includes/class-velocity-addons-maintenance-mode.php
+++ includes/class-velocity-addons-maintenance-mode.php
@@ -21,15 +21,18 @@
  * @subpackage Velocity_Addons/includes
  * @author     Velocity <bantuanvelocity@gmail.com>
  */
-class Velocity_Addons_Maintenance_Mode {
-    public function __construct() {
+class Velocity_Addons_Maintenance_Mode
+{
+    public function __construct()
+    {
         if (get_option('maintenance_mode')) {
             add_action('wp', array($this, 'check_maintenance_mode'));
         }
     }
 
-    public function check_maintenance_mode() {
-        if (!current_user_can('manage_options') && !is_admin()) {
+    public function check_maintenance_mode()
+    {
+        if (!current_user_can('manage_options') && !is_admin() && !is_page('myaccount')) {
             wp_die('<h1>Maintenance Mode</h1><p>We are currently performing maintenance. Please check back later.</p>', 'Maintenance Mode');
         }
     }
