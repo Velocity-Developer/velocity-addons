808359bcfeea398ec250cf650996bc55167f1051
aadiityaak <aadiityaak@gmail.com>
2024-09-19 11:33:11 +0700

    fix zip structure

--- src/zip.js
+++ src/zip.js
@@ -1,19 +1,58 @@
-const fs = require("fs");
+const fs = require("fs-extra");
 const archiver = require("archiver");
 const path = require("path");
+const glob = require("glob");
 const packageJson = require("../package.json");
 
 // Nama folder dan file zip yang akan dibuat
-const pluginName = "velocity-addons";
+const pluginName = packageJson.name;
 const pluginVersion = packageJson.version;
 const outputFolder = path.join(__dirname, "../dist");
+const tempFolder = path.join(outputFolder, pluginName);
 const outputFileName = `${pluginName}-${pluginVersion}.zip`;
 const outputPath = path.join(outputFolder, outputFileName);
 
-// Pastikan folder 'dist' ada
+// Fungsi untuk menghapus folder
+const clearFolder = (folderPath) => {
+  if (fs.existsSync(folderPath)) {
+    fs.rmSync(folderPath, { recursive: true, force: true });
+  }
+};
+
+// Pastikan folder 'dist' ada dan kosongkan isinya
 if (!fs.existsSync(outputFolder)) {
   fs.mkdirSync(outputFolder);
+} else {
+  clearFolder(outputFolder);
 }
+
+// Hapus folder sementara jika sudah ada
+if (fs.existsSync(tempFolder)) {
+  clearFolder(tempFolder);
+}
+
+// Buat folder sementara berdasarkan nama plugin
+fs.mkdirSync(tempFolder, { recursive: true });
+
+// Salin file ke dalam folder sementara
+const files = glob.sync("**/*", {
+  cwd: path.join(__dirname, ".."),
+  ignore: [
+    "dist/**", // Abaikan folder dist
+    "src/**", // Abaikan folder src
+    "node_modules/**", // Abaikan folder node_modules
+    "package.json", // Abaikan file package.json
+    "package-lock.json", // Abaikan file package-lock.json
+  ],
+});
+
+files.forEach((file) => {
+  const filePath = path.join(__dirname, "..", file);
+  const destPath = path.join(tempFolder, file);
+
+  // Salin file atau folder
+  fs.copySync(filePath, destPath);
+});
 
 // Membuat file output stream
 const output = fs.createWriteStream(outputPath);
@@ -25,6 +64,9 @@ const archive = archiver("zip", {
 output.on("close", function () {
   console.log(archive.pointer() + " total bytes");
   console.log("File zip telah dibuat: " + outputFileName);
+
+  // Hapus folder sementara setelah zip selesai
+  clearFolder(tempFolder);
 });
 
 // Event listener saat error
@@ -35,8 +77,8 @@ archive.on("error", function (err) {
 // Mulai membuat file zip
 archive.pipe(output);
 
-// Tambahkan semua file dalam direktori plugin ke zip
-archive.directory(__dirname, false);
+// Tambahkan folder sementara ke arsip zip dengan nama folder di dalam zip
+archive.directory(tempFolder, pluginName);
 
 // Akhiri proses zip
 archive.finalize();
