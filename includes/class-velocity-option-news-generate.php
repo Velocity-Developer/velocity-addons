0ae38de18716dc67710a8e705bf4795089d165de
irawanyulianto <irawanyulianto77@gmail.com>
2025-09-30 13:01:47 +0700

    Jika posts sudah ada, maka update kategorinya

--- includes/class-velocity-option-news-generate.php
+++ includes/class-velocity-option-news-generate.php
@@ -138,10 +138,18 @@ class Velocity_Addons_News
         ob_start();
         $existing_post = self::cek_posts_by_title($title);
         if ($existing_post) {
-            // Jika post sudah ada, tampilkan pesan
-            echo '<p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#dd0000" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
-                </svg> Gagal import! Post dengan judul "' . esc_html($title) . '" sudah ada.</p>';
+            // Update category kalau disediakan
+            if ($category) {
+                wp_set_post_terms($existing_post->ID, $category, 'category');
+                echo '<p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#51b20c" class="bi bi-check-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                    <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
+                    </svg> Post dengan judul "' . esc_html($title) . '" sudah ada, kategori diperbarui.</p>';
+            } else {
+                echo '<p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#dd0000" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+                    </svg> Gagal import! Post dengan judul "' . esc_html($title) . '" sudah ada.</p>';
+            }
         } else {
             $post_data = array(
                 'post_title'    => wp_strip_all_tags($title),
