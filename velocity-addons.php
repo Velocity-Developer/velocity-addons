fd016fb654541b658ecbc485844f99968a5a4ff7
adityathok <aditya0thok@gmail.com>
2026-06-19 15:15:09 +0700

    feat: depcated function justg_share

--- velocity-addons.php
+++ velocity-addons.php
@@ -41,6 +41,7 @@ define('PLUGIN_DIR', plugin_dir_path(__DIR__));
 define('PLUGIN_FILE', plugin_basename(__FILE__));
 define('PLUGIN_BASE_NAME', plugin_basename(__DIR__));
 define('VELOCITY_ADDONS_PLUGIN_DIR_URL', plugin_dir_url(__FILE__));
+require_once plugin_dir_path(__FILE__) . 'includes/depracated.php';
 
 /**
  * The code that runs during plugin activation.
